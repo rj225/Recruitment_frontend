b11b5bea2bff5df0243e4d721808791093dfdbbf
Rishabh Raj <rajrishabh225@gmail.com>
2024-06-15 16:50:12 +0530

    working register page

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -3,24 +3,40 @@ import axios from 'axios';
 import { Link } from 'react-router-dom';
 import AOS from 'aos';
 import { useEffect } from 'react';
+import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
 
 const Register = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [companyName, setCompanyName] = useState('');
+  const navigate = useNavigate();
 
   useEffect(() => {
     AOS.init({ duration: 1000 });
   }, []);  
 
-  const handleRegister = async () => {
+  const handleRegister = async (e) => {
+    e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:5000/api/users/register', { email, password, companyName });
-      console.log(response.data);
+      const payload = {
+        c_name: companyName,
+        c_email: email,
+        password: password
+      };
+    
+     await toast.promise(axios.post(`${process.env.REACT_APP_BASIC_API_URL}/register`, payload),
+      {
+        pending: 'Registering in...',
+        success: "Registered",
+        error: 'Registration failed.',
+      })
+      navigate("/login");
     } catch (err) {
       console.error(err);
     }
   };
+  
 
   return (
     <div class="h-screen md:flex">
@@ -54,9 +70,9 @@ const Register = () => {
               onChange={(e) => setCompanyName(e.target.value)} />
           </div>
           <div class="flex items-center border-2  border-red-300 sm:py-2 sm:px-3 py-1 px-2 rounded-2xl mb-4">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-              <path fill-rule="evenodd" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
-            </svg>
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
+              </svg>
             <input
               class="pl-2 outline-none w-full border-none"
               type="text"
@@ -65,12 +81,12 @@ const Register = () => {
               onChange={(e) => setEmail(e.target.value)} />
           </div>
           <div class="flex items-center border-2  border-red-300 sm:py-2 sm:px-3 py-1 px-2 rounded-2xl mb-4">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-              <path fill-rule="evenodd" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
-            </svg>
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
+              </svg>
             <input
               class="pl-2 outline-none w-full border-none"
-              type="text"
+              type="password"
               placeholder="Your Password"
               value={password}
               onChange={(e) => setPassword(e.target.value)} />
